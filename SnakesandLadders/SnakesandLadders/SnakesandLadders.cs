71e6cb035aff76337d5d281223faf5325b67c72b
Emmanuel Okuchukwu <emmanza2@gmail.com>
2018-10-14 17:54:29 +0100

    Update

---
+++ SnakesandLadders/SnakesandLadders/SnakesandLadders.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakesandLaddersUT
+{
+    public class Snakes_and_Ladders
+    {
+        string _Game;
+        int _token;
+
+        public Snakes_and_Ladders(string Game, int token)
+        {
+            _Game = Game;
+            _token = token;
+        }
+
+        public int token { get { return _token; } }
+
+        public void Game(string Game)
+        {
+
+        }
+    }
+}
+
+
